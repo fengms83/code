78792c4f6a9fde6b39e3f9b063aad333c31424ec
fengms83 <fengms@qq.com>
2016-09-16 15:43:03 +0800

    修正代码结构

---
+++ c/dsaac/ch03/test_cases.c
@@ -0,0 +1,38 @@
+#include "utils.h"
+#include "list.h"
+
+
+void test_is_equal(void)
+{
+	int i = 0;
+	int len = 5;
+
+	struct list *p = NULL;
+	struct list *q = NULL;
+
+	/* 头节点不存储数据 */
+	struct list *head = (struct list *)malloc(sizeof(struct list));
+	head->next = NULL;
+
+	for (i = 0; i < len; ++i)
+	{
+		p = head;
+		while(p->next)
+			p = p->next;
+
+		q = (struct list *)malloc(sizeof(struct list));
+		q->value = i;
+		q->next = NULL;
+
+		p->next = q;
+	}
+
+	int array1[] = {0,1,2,3,4};
+	int len1 = sizeof(array1) / sizeof(array1[0]);
+
+	int array2[] = {0,1,2,3,4,5,6};
+	int len2 = sizeof(array2) / sizeof(array2[0]);
+
+	assert(TRUE  == is_equal(head, array1, len1));
+	assert(FALSE == is_equal(head, array2, len2));
+}
--- c/dsaac/ch03/utils.c
+++ c/dsaac/ch03/utils.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
-#include <assert.h>
-#include "list.h"
-
-typedef int BOOL;
-#define TRUE  1
-#define FALSE 0
+#include "utils.h"
 
 BOOL is_equal(struct list* head, int* array, int len)
 {
@@ -13,7 +7,7 @@ BOOL is_equal(struct list* head, int* array, int len)
 	while((i < len) && (head->next))
 	{
 		head = head->next;
-		
+
 		if (array[i] != head->value)
 			return FALSE;
 
@@ -24,39 +18,4 @@ BOOL is_equal(struct list* head, int* array, int len)
 		return FALSE;
 
 	return TRUE;
-}
-
-void test_is_equal(void)
-{
-	int i = 0;
-	int len = 5;
-
-	struct list *p = NULL;
-	struct list *q = NULL;
-
-	/* 头节点不存储数据 */
-	struct list *head = (struct list *)malloc(sizeof(struct list));
-	head->next = NULL;
-
-	for (i = 0; i < len; ++i)
-	{
-		p = head;
-		while(p->next)
-			p = p->next;
-
-		q = (struct list *)malloc(sizeof(struct list));
-		q->value = i;
-		q->next = NULL;
-
-		p->next = q;
-	}
-
-	int array1[] = {0,1,2,3,4};
-	int len1 = sizeof(array1) / sizeof(array1[0]);
-
-	int array2[] = {0,1,2,3,4,5,6};
-	int len2 = sizeof(array2) / sizeof(array2[0]);
-
-	assert(TRUE  == is_equal(head, array1, len1));
-	assert(FALSE == is_equal(head, array2, len2));
 }
