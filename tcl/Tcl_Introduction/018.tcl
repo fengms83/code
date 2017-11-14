b4aea17fd2d1a91c9292dd73f20a334a44616a40
fengms83 <fengms@qq.com>
2017-11-14 23:27:29 +0800

    add string operations

---
+++ tcl/Tcl_Introduction/018.tcl
@@ -0,0 +1,5 @@
+
+set string "this is my test string"
+puts "There are [string length $string] characters in \"$string\""
+puts "[string index $string 1] is the second character in \"$string\""
+puts "\"[string range $string 5 10]\" are characters between the 5th and 10th"
